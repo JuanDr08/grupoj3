05c761145725c591d97d3f7efcd2b004c44896bf
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-01-20 13:12:45 -0500

    Final 0 errores

--- Trabajo.py
+++ Trabajo.py
@@ -21,7 +21,7 @@ while Encendido:
     os.system('cls')
     print(titulo)
     print(opciones)
-    opcion = int(input("-(Entero): "))
+    opcion = int(input("-(Numero): "))
     if (opcion == 1):
         os.system("cls")
         nombreEquip = str(input("Ingrese el nombre del equipo: ")).upper()             # pedir nombre
@@ -31,7 +31,7 @@ while Encendido:
                 if (i >= 1):
                     if (Equipos[i-1][0] == Equipos[i][0]):
                         os.system("cls")
-                        print("Debe registrar dos equipos diferentes, el segundo registrado sera eliminado")
+                        print("Debe registrar dos equipos diferentes, el que ha escrito ya se encontraba creado")
                         Equipos.pop(i)
                         os.system("pause")
     elif(opcion == 2):
@@ -91,71 +91,82 @@ while Encendido:
                 os.system("pause")
     elif(opcion == 3):
         os.system("cls")
-        print(tabla)
-        print(tabulate(Equipos,headers=["Equipo", "PJ", "PG", "PP", "PE", "GF", "GC", "TP"]))
-        os.system("pause")
+        if(len(Equipos) < 1):
+            print("No hay equpos registrados en la liga, no hay posiciones")
+            os.system("pause")
+        else:
+            print(tabla)
+            print(tabulate(Equipos,headers=["Equipo", "PJ", "PG", "PP", "PE", "GF", "GC", "TP"]))
+            os.system("pause")
     elif(opcion == 4):
+        os.system("cls")
     # ORDEN SEGUN MAS GOLES ANOTADOS
-        for i,item in enumerate(Equipos):
-            for j in range(int(i+1), len(Equipos),1):
-                if (Equipos[i][5] < Equipos[j][5]):
-                    aux = Equipos[i]
-                    Equipos[i] = Equipos[j]
-                    Equipos[j] = aux
-            if (i == ((len(Equipos))-1)):
-                if (Equipos[i - ((len(Equipos))-1)][5] == Equipos[j - ((len(Equipos))-2)][5]):
-                    os.system("cls")
-                    print("HAY DOS EQUIPOS CON LA MISMA CANTIDAD DE GOLES ANOTADOS")
-                    print(f"A. Los equipos que mas goles anotaron fueron el equipo: {Equipos[i-((len(Equipos))-1)][0]} con un total de {Equipos[i-((len(Equipos))-1)][5]} goles")
-                    print(f" y el equipo: {Equipos[i-((len(Equipos))-2)][0]} con un total de {Equipos[i-((len(Equipos))-2)][5]} goles")
-                    os.system("pause")
-                else:
-                    print("A. El equipo que mas goles anoto fue: ", Equipos[i-((len(Equipos))-1)][0], f"con un total de {Equipos[i-((len(Equipos))-1)][5]} goles")
-                    os.system("pause")
-    # ORDEN SEGUN MAS PUNTOS OBTENIDOS
-        for i,item in enumerate(Equipos):
-            for j in range(int(i+1), len(Equipos),1):
-                if (Equipos[i][7] < Equipos[j][7]):
-                    aux = Equipos[i]
-                    Equipos[i] = Equipos[j]
-                    Equipos[j] = aux
-            if (i == ((len(Equipos))-1)):
-                if (Equipos[i - ((len(Equipos))-1)][7] == Equipos[j - ((len(Equipos))-2)][7]):
-                    os.system("cls")
-                    print("HAY DOS EQUIPOS QUE COMPARTEN LA MISMA CANTIDAD DE PUNTOS")
-                    print(f"B. Los equipos que mas puntos tienen son el equipo: {Equipos[i-((len(Equipos))-1)][0]} con un total de {Equipos[i-((len(Equipos))-1)][7]} puntos")
-                    print(f" y el equipo: {Equipos[i-((len(Equipos))-2)][0]} con un total de {Equipos[i-((len(Equipos))-2)][7]} puntos")
-                    os.system("pause")
-                else:
-                    print("B. El equipo con mas puntos fue: ", Equipos[i-((len(Equipos))-1)][0], f"con un total de {Equipos[i-((len(Equipos))-1)][7]} puntos")
-                    os.system("pause")
-    # ORDEN SEGUN PARTIDOS GANADOS
-    # TOTAL DE GOLES DE LOS EQUIPOS
-    # PROMEDIO DE GOLES ANOTADOS (SUMA DE GOLES / SUMA PARTIDOS TOTALES)
-        totalGoles = 0
-        totalPartidos = 0
-        for i,item in enumerate(Equipos):
-            totalGoles += item[5]
-            totalPartidos += item[1]
-            for j in range(int(i+1), len(Equipos),1):
-                if (Equipos[i][2] < Equipos[j][2]):
-                    aux = Equipos[i]
-                    Equipos[i] = Equipos[j]
-                    Equipos[j] = aux
-            if (i == ((len(Equipos))-1)):
-                if ((Equipos[i - ((len(Equipos))-1)][2]) == (Equipos[j - ((len(Equipos))-2)][2])):
-                    os.system("cls")
-                    print("HAY DOS EQUIPOS CON LOS MISMOS PARTIDOS GANADOS")
-                    print("C. los equipos con mas partidos ganados fueron: ", Equipos[i-((len(Equipos))-1)][0], f" con un total de {Equipos[i-((len(Equipos))-1)][2]}  partidos ganados")
-                    print(" y el equipo: ", Equipos[i-((len(Equipos))-2)][0], f" con un total de {Equipos[i-((len(Equipos))-2)][2]}  partidos ganados")
-                    os.system("pause")
-                else:
-                    print("C. El equipo con mas partidos ganados fue: ", Equipos[i-((len(Equipos))-1)][0], f" con un total de {Equipos[i-((len(Equipos))-1)][2]} partidos ganados")
-                    os.system("pause")
-                os.system("cls")
-                print("D. El total de goles anotados por todos los equipos fue: ", totalGoles)
-                print("E. El promedio de goles por partido es: ", totalGoles/totalPartidos )
-                os.system("pause")
+        if (len(Equipos) < 2):
+            print("Tienen que haber almenos 2 equipos registrados para poder tener alguna estadistica")
+            os.system("pause")
+        else:
+            for i,item in enumerate(Equipos):
+                for j in range(int(i+1), len(Equipos),1):
+                    if (Equipos[i][5] < Equipos[j][5]):
+                        aux = Equipos[i]
+                        Equipos[i] = Equipos[j]
+                        Equipos[j] = aux
+                if (i == ((len(Equipos))-1)):
+                    if ((Equipos[i - ((len(Equipos))-1)][5] == 0) and (Equipos[j - ((len(Equipos))-2)][5] == 0)):
+                        print("A. No hay un equipo con mas goles, ya que ninguno de los equipos ha metido ni 1 solo gol, puede observarlo en las estadisticas")
+                    elif (Equipos[i - ((len(Equipos))-1)][5] == Equipos[j - ((len(Equipos))-2)][5]):
+                        os.system("cls")
+                        print("A. HAY DOS EQUIPOS CON LA MISMA CANTIDAD DE GOLES ANOTADOS")
+                        print(f"Los equipos que mas goles anotaron fueron el equipo: {Equipos[i-((len(Equipos))-1)][0]} con un total de {Equipos[i-((len(Equipos))-1)][5]} goles")
+                        print(f" y el equipo: {Equipos[i-((len(Equipos))-2)][0]} con un total de {Equipos[i-((len(Equipos))-2)][5]} goles")
+                    else:
+                        os.system("cls")
+                        print("A. El equipo que mas goles anoto fue: ", Equipos[i-((len(Equipos))-1)][0], f"con un total de {Equipos[i-((len(Equipos))-1)][5]} goles")
+        # ORDEN SEGUN MAS PUNTOS OBTENIDOS
+            for i,item in enumerate(Equipos):
+                for j in range(int(i+1), len(Equipos),1):
+                    if (Equipos[i][7] < Equipos[j][7]):
+                        aux = Equipos[i]
+                        Equipos[i] = Equipos[j]
+                        Equipos[j] = aux
+                if (i == ((len(Equipos))-1)):
+                    if ((Equipos[i - ((len(Equipos))-1)][7] == 0) and (Equipos[j - ((len(Equipos))-2)][7] == 0)):
+                        print("B. No hay un equipo con mas puntos, ya que ninguno de los equipos ha logrado obtener almenos 1 punto o no han jugado, puede observarlo en la tabla")
+                    elif (Equipos[i - ((len(Equipos))-1)][7] == Equipos[j - ((len(Equipos))-2)][7]):
+                        print("B. HAY DOS EQUIPOS QUE COMPARTEN LA MISMA CANTIDAD DE PUNTOS")
+                        print(f"Los equipos que mas puntos tienen son el equipo: {Equipos[i-((len(Equipos))-1)][0]} con un total de {Equipos[i-((len(Equipos))-1)][7]} puntos")
+                        print(f" y el equipo: {Equipos[i-((len(Equipos))-2)][0]} con un total de {Equipos[i-((len(Equipos))-2)][7]} puntos")
+                    else:
+                        print("B. El equipo con mas puntos fue: ", Equipos[i-((len(Equipos))-1)][0], f"con un total de {Equipos[i-((len(Equipos))-1)][7]} puntos")
+        # ORDEN SEGUN PARTIDOS GANADOS
+        # TOTAL DE GOLES DE LOS EQUIPOS
+        # PROMEDIO DE GOLES ANOTADOS (SUMA DE GOLES / SUMA PARTIDOS TOTALES)
+            totalGoles = 0
+            totalPartidos = 0
+            for i,item in enumerate(Equipos):
+                totalGoles += item[5]
+                totalPartidos += item[1]
+                for j in range(int(i+1), len(Equipos),1):
+                    if (Equipos[i][2] < Equipos[j][2]):
+                        aux = Equipos[i]
+                        Equipos[i] = Equipos[j]
+                        Equipos[j] = aux
+                if (i == ((len(Equipos))-1)):
+                    if ((Equipos[i - ((len(Equipos))-1)][2] == 0) and (Equipos[j - ((len(Equipos))-2)][2] == 0)):
+                        print("C. No hay un equipo con mas partidos ganados ya que ningun equipo ha ganado almenos un partido, puede observarlo en la tabla")
+                    elif ((Equipos[i - ((len(Equipos))-1)][2]) == (Equipos[j - ((len(Equipos))-2)][2])):
+                        print("C. HAY DOS EQUIPOS CON LOS MISMOS PARTIDOS GANADOS")
+                        print("los equipos con mas partidos ganados fueron: ", Equipos[i-((len(Equipos))-1)][0], f" con un total de {Equipos[i-((len(Equipos))-1)][2]}  partidos ganados")
+                        print(" y el equipo: ", Equipos[i-((len(Equipos))-2)][0], f" con un total de {Equipos[i-((len(Equipos))-2)][2]}  partidos ganados")
+                    else:
+                        print("C. El equipo con mas partidos ganados fue: ", Equipos[i-((len(Equipos))-1)][0], f" con un total de {Equipos[i-((len(Equipos))-1)][2]} partidos ganados")
+                    print("D. El total de goles anotados por todos los equipos fue: ", totalGoles, " Goles")
+                    if (totalPartidos == 0):
+                       print("E. No se ha jugado ningun partido, por lo tanto el promedio es 0") 
+                       os.system("pause")
+                    else:
+                        print("E. El promedio de goles por partido es: ", totalGoles/totalPartidos )
+                        os.system("pause")
     
     else:
         print("Escoja una opcion valida")
